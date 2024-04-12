9259c785e702c61da1c4a17f30f18f0f11c6f73b
Ingrid Lima <ibezerradelima@gmail.com>
2024-04-12 16:16:21 -0300

    :recycle: refactor: sectionTitle background
    refactored to resize based on text.
    
    #7

--- src/components/Home/SectionTitle.tsx
+++ src/components/Home/SectionTitle.tsx
@@ -1,25 +1,14 @@
 "use client";
-import Image from "next/image";
-import { dimensions } from "../utils/layout";
 
 export default function SectionTitle({ title }: Readonly<{ title: string }>) {
   return (
-    <div className="mb-16 relative flex items-center justify-center">
-      <Image
-        src="background_blue.svg"
-        alt="background image"
-        width={dimensions.width / 9}
-        height={dimensions.height / 8}
-      />
+    <div className="mb-16 flex items-center justify-center">
+      <div className="flex items-center bg-customBlue p-6 rounded-lg rotate-2">
+        <h2 className="text-4xl invisible">{title}</h2>
+      </div>
 
-      <div
-        className="absolute bg-black py-2 rounded"
-        style={{
-          height: dimensions.height / 11.2,
-          width: dimensions.width / 9.8,
-        }}
-      >
-        <h2 className="text-5xl text-center">{title}</h2>
+      <div className="absolute flex p-4 rounded-lg bg-black">
+        <h2 className="text-4xl">{title}</h2>
       </div>
     </div>
   );
