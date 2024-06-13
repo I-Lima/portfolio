228f595d43b2a87ed2dbb9f28d95e3eda17cac5b
Ingrid Lima <ibezerradelima@gmail.com>
2024-06-13 08:31:42 -0300

    :bug: fix: Github API access limited (#25)
    * :bug: fix: Github API access limited
    
    * fix: ESlint error

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -4,6 +4,9 @@ import { GITHUB } from "./constant/urls";
 const axiosAPIGithub = axios.create({
   baseURL: "https://api.github.com/",
   timeout: 100000,
+  headers: {
+    Authorization: `Bearer ${process.env.GITHUB_KEY}`,
+  },
 });
 
 export async function getGithubRepositories() {
