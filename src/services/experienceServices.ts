0bb3f91aabd7f4d99cbbbfdd172cc7b577e41eb4
Ingrid Lima <ibezerradelima@gmail.com>
2024-06-13 07:40:05 -0300

    15 refactor the experiences section to get data into firebase (#24)
    * Create experiencesDAO
    
    * :sparkles: feat: Create experiences route
    
    * Create  experienceRepository
    
    * Add experiences props
    
    * :bug: fix: data props return
    
    * Create experienceServices
    
    * :recycle: refactor: Experience section to get data in firebase
    
    * fix: Eslint errors

---
+++ src/services/experienceServices.ts
@@ -0,0 +1,94 @@
+import { getExperienceData } from "@/Repositories/experienceRepository";
+import { dataProps } from "@/types/api";
+import { experienceHistoryProps, experienceProps } from "@/types/dao";
+import { parseDateString } from "@/utils/general";
+import to from "await-to-js";
+import _ from "lodash";
+
+class ExperienceServices {
+  /**
+   * Retrieves a preview of experience data by fetching the experience data from the server,
+   * sorting it, and formatting it into a specific structure.
+   *
+   * @return {Promise<experienceProps[] | null>} An array of experience data objects with formatted history.
+   * If there is an error or no data is returned from the server, null is returned.
+   */
+  async getPreviewExperiencesData(): Promise<experienceProps[] | null> {
+    const [error, data] = await to(getExperienceData());
+    if (error || !data) return null;
+
+    const sortedDocuments: experienceProps[] = _.orderBy(
+      (data as dataProps).data,
+      "output",
+    );
+    const newSortedDocuments = sortedDocuments.map((document) => {
+      const historyArray = Object.values(document.history);
+      const orderedHistory = _.orderBy(
+        historyArray,
+        (item) => parseDateString(item.output),
+        ["desc"],
+      );
+
+      const updatedHistory = orderedHistory.map((item) => {
+        return { ...item, output: item.output || "currently" };
+      });
+
+      return { ...document, history: updatedHistory };
+    });
+
+    const firstHistory = newSortedDocuments[0].history;
+    if (firstHistory.length === 2) return [newSortedDocuments[0]];
+
+    const historyReturn: experienceHistoryProps[] = [];
+    if (firstHistory.length >= 2) {
+      historyReturn.push(firstHistory[0]);
+      historyReturn.push(firstHistory[1]);
+
+      return [
+        { ...newSortedDocuments[0], history: historyReturn },
+      ] as experienceProps[];
+    }
+
+    const dataReturn: experienceProps[] = [
+      {
+        ...newSortedDocuments[0],
+        history: [firstHistory[0]],
+      },
+    ];
+
+    if (newSortedDocuments[1]) {
+      dataReturn.push({
+        ...newSortedDocuments[1],
+        history: [newSortedDocuments[1].history[0]],
+      });
+    }
+
+    return dataReturn;
+  }
+
+  /**
+   * Retrieves all experience data from the server, sorts it, and formats it.
+   *
+   * @return {Promise<experienceProps[] | null>} An array of experience data objects with formatted history.
+   * If there is an error or no data is returned from the server, null is returned.
+   */
+  async getAllExperienceData() {
+    const [error, data] = await to(getExperienceData());
+    if (error) return null;
+
+    const sortedDocuments: experienceProps[] = _.orderBy(
+      (data as dataProps).data,
+      "output",
+    );
+    const newSortedDocuments = sortedDocuments.map((item) => {
+      const historyArray = Object.keys(item.history).map((key) =>
+        JSON.parse(key),
+      );
+      return { ...item, history: historyArray as experienceHistoryProps[] };
+    });
+
+    return newSortedDocuments as experienceProps[];
+  }
+}
+
+export default new ExperienceServices();
--- src/types/dao.ts
+++ src/types/dao.ts
@@ -8,3 +8,22 @@ export type aboutProps = {
   description: string[];
   skills: aboutSkillProps[];
 };
+
+// ------------------- EXPERIENCES DAO --------------------
+export type experienceHistoryProps = {
+  role: string;
+  description: string;
+  tags: string[];
+  entrance: string;
+  output: string | null;
+  project_website: string | null;
+  type: string;
+};
+
+export type experienceProps = {
+  id: string;
+  enterprise: string;
+  output: number | null;
+  company_website: string;
+  history: experienceHistoryProps[];
+};
