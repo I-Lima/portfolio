7d281254e4378a3233324ca9d008c39270c71cc2
Ingrid Lima <ibezerradelima@gmail.com>
2024-06-24 20:21:14 -0300

    Create ButtonCustom component
    #22

---
+++ src/components/Experiences/Button.tsx
@@ -0,0 +1,13 @@
+export default function ButtonCustom({
+  onClick,
+  title,
+}: Readonly<{ onClick: () => void; title: string }>) {
+  return (
+    <div
+      className="rounded-3xl border-white border-2 border-dashed flex w-44 justify-center hover:cursor-pointer"
+      onClick={onClick}
+    >
+      <p className="text-center text-xl my-2">{title}</p>
+    </div>
+  );
+}
