1e011f276e1af948e84283d8e7ee1ba3a669d07b
Ingrid Lima <ibezerradelima@gmail.com>
2024-07-07 18:43:15 -0300

    Add SwitchDarkMode to home screen
    #17

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,24 +6,31 @@ import { dimensions } from "../utils/layout";
 import About from "@/components/Home/About";
 import Experiences from "@/components/Home/Experiences";
 import Projects from "@/components/Home/Projects";
+import SwitchDarkMode from "@/components/SwitchDarkMode";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen min-w-screen flex-col items-center">
       <div className="flex flex-col h-screen max-w-screen pt-24">
-        <Banner />
+        <div className="flex justify-end w-screen pr-20">
+          <SwitchDarkMode />
+        </div>
 
-        <div className="flex flex-col items-center mt-20">
-          <p className="text-3xl mb-2 pb-1" style={{ borderBottomWidth: 1 }}>
-            More Information
-          </p>
+        <div>
+          <Banner />
 
-          <Image
-            src="arrow.svg"
-            alt="arrow icon"
-            width={dimensions.width / 80}
-            height={dimensions.width / 80}
-          />
+          <div className="flex flex-col items-center mt-20">
+            <p className="text-3xl mb-2 pb-1" style={{ borderBottomWidth: 1 }}>
+              More Information
+            </p>
+
+            <Image
+              src="arrow.svg"
+              alt="arrow icon"
+              width={dimensions.width / 80}
+              height={dimensions.width / 80}
+            />
+          </div>
         </div>
       </div>
 
