7dc6c64eebf7f7b8908bb030349e3c45d08221a7
Ingrid Lima <ibezerradelima@gmail.com>
2024-07-07 18:44:38 -0300

    Create SwitchDarkMode component
    #17

--- src/components/SwitchDarkMode.tsx
+++ src/components/SwitchDarkMode.tsx
@@ -1,19 +1,32 @@
-import { useState } from "react";
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { MdDarkMode, MdSunny } from "react-icons/md";
 
 function SwitchDarkMode() {
-  const [isOn, setIsOn] = useState(false);
+  const [isDark, setIsDark] = useState(false);
+  const { setTheme, theme } = useTheme();
+
+  const handleToggle = () => {
+    setIsDark(!isDark);
+    setTheme(isDark ? "light" : "dark");
+  }
+
+  useEffect(() => {
+    setIsDark(theme === "dark");
+  }, [theme]);
 
   return (
     <div className="flex flex-row gap-1 items-center">
       <MdSunny size={36} className="text-black" />
 
       <div
-        onClick={() => setIsOn(!isOn)}
-        className={`w-16 h-8 flex items-center border-black border-2 bg-white rounded-full p-1 cursor-pointer transition-colors duration-300 ${isOn ? 'bg-green-500' : ''}`}
+        onClick={handleToggle}
+        className={`w-16 h-8 flex items-center border-black border-2 bg-white rounded-full p-1 cursor-pointer transition-colors duration-300 ${isDark ? 'bg-green-500' : ''}`}
       >
         <div
-          className={`bg-black w-6 h-6 rounded-full shadow-md transform transition-transform duration-300 ${isOn ? 'translate-x-7' : ''
+          className={`bg-black w-6 h-6 rounded-full shadow-md transform transition-transform duration-300 ${isDark ? 'translate-x-7' : ''
             }`}
         ></div>
       </div>
