3c4db9171a2cfb72443c7ce9adfdf2c99d91f901
Ingrid Lima <ibezerradelima@gmail.com>
2025-01-26 12:47:33 -0300

    Configure i18n
    #52

---
+++ src/types/dictionaries.d.ts
@@ -0,0 +1,30 @@
+export type aboutProps = {
+  section: string;
+};
+export type bannerProps = {
+  hi: string;
+  myName: string;
+  iAm: string;
+};
+export type contactProps = {
+  section: string;
+  description: string;
+};
+export type experiencesProps = {
+  section: string;
+};
+export type homeProps = {
+  moreInfo: string;
+};
+export type projectsProps = {
+  section: string;
+};
+
+export type dictionariesProps = {
+  about: aboutProps;
+  banner: bannerProps;
+  contact: contactProps;
+  Experiences: experiencesProps;
+  home: homeProps;
+  projects: projectsProps;
+};
